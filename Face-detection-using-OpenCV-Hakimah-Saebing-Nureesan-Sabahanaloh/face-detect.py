0eedf3d12a10f65c11143ce09009abb8362242fc
Hqkimah <96815665+Hqkimah@users.noreply.github.com>
2022-04-12 19:57:19 +0700

    Update face-detect.py

--- Face-detection-using-OpenCV-Hakimah-Saebing-Nureesan-Sabahanaloh/face-detect.py
+++ Face-detection-using-OpenCV-Hakimah-Saebing-Nureesan-Sabahanaloh/face-detect.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 
-img = cv.imread('smile.jpg')
+img = cv.imread('smile.jpg') 
 face_model = cv.CascadeClassifier('face-detect-model.xml')
 gray_scale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 faces = face_model.detectMultiScale(gray_scale)
 
-for (x,y,w,h) in faces: 
-    cv.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)  #RGB -> BGR
+for (x, y, w, h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) #RGB -> BGR
 
 
-cv.imshow('imange', img)
-cv.waitkey(0)
-cv.destroyAllWindpws()
+cv.imshow('imge',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
